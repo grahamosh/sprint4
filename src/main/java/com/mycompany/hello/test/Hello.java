56a261cbb5c9dadd386bcc7ab246386ece09f96e
Graham <grahamosh@outlook.com>
2017-02-20 10:51:46 -0500

    sprint 4 files

---
+++ src/main/java/com/mycompany/hello/test/Hello.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.hello.test;
+
+/**
+ *
+ * @author AHOLLIDG
+ */
+public class Hello {
+    public static void main(String[] args) {
+        Hello helloworld = new Hello();
+        helloworld.sayHello();    
+    }
+    public String sayHello(){
+        return "Hello World";
+    }
+}
